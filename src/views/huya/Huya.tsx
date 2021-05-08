f3b60dda626c6f3fa37e6b50d224a34cf554c9cd
jesse <szx961102@gmail.com>
2021-05-08 08:22:26 +0800

    refactor huya

--- src/views/huya/Huya.tsx
+++ src/views/huya/Huya.tsx
@@ -12,6 +12,7 @@ import { useAppDispatch, useAppSelector } from '../../hook'
 import { RootState } from '../../store'
 import { CommonResult } from '../../components/CommonResult'
 import { PageInfo } from '../../components/PageInfo'
+import { CommonView } from '../../components/CommonView'
 
 const huya = () => {
   let width = document.querySelector('body')!.offsetWidth
@@ -66,90 +67,21 @@ const huya = () => {
     setRowCount(totalCount / NUM_COLUMNS)
   }, [totalCount])
 
-  const Cell = (props: any) => {
-    const { columnIndex, rowIndex, style } = props
-    const itemIndex = rowIndex * NUM_COLUMNS + columnIndex
-
-    const i = huyaData[itemIndex] || {}
-
-    return (
-      <div
-        className="flex flex-col  items-center"
-        key={itemIndex}
-        style={style}
-      >
-        <Link
-          to={`/huya/${i.profileRoom}`}
-          className="flex flex-col justify-center  items-start w-44 "
-        >
-          <img src={i.screenshot} className="h-32 w-44"></img>
-          <div className="flex flex-col">
-            <div className="text-sm">{i.nick}</div>
-            <div className="text-sm">{i.introduction}</div>
-          </div>
-        </Link>
-      </div>
-    )
-  }
-
-  const innerElementType = forwardRef<any, any>(({ style, ...rest }, ref) => (
-    <div
-      ref={ref}
-      style={{
-        ...style,
-        position: 'relative',
-        margin: 'auto',
-      }}
-      {...rest}
-    />
-  ))
-
   return (
-    <div className="h-screen">
-      <div
-        className=" bg-gray-50 text-xl flex items-center"
-        style={{ height: 70 }}
-      >
-        斗鱼
-      </div>
-      <AutoSizer>
-        {({ height, width }) => (
-          <InfiniteLoader
-            isItemLoaded={isItemLoaded}
-            itemCount={totalCount}
-            loadMoreItems={loadMoreItems}
-          >
-            {({ onItemsRendered, ref }) => (
-              <Grid
-                className="bg-gray-200"
-                columnCount={NUM_COLUMNS}
-                columnWidth={width / NUM_COLUMNS - 10}
-                height={height - 70}
-                rowCount={rowCount}
-                rowHeight={200}
-                innerElementType={innerElementType}
-                onItemsRendered={(gridProps) => {
-                  onItemsRendered({
-                    overscanStartIndex:
-                      gridProps.overscanRowStartIndex * NUM_COLUMNS,
-                    overscanStopIndex:
-                      gridProps.overscanRowStopIndex * NUM_COLUMNS,
-                    visibleStartIndex:
-                      gridProps.visibleRowStartIndex * NUM_COLUMNS,
-                    visibleStopIndex:
-                      gridProps.visibleRowStopIndex * NUM_COLUMNS,
-                  })
-                }}
-                ref={ref}
-                width={width}
-              >
-                {Cell}
-              </Grid>
-            )}
-          </InfiniteLoader>
-        )}
-      </AutoSizer>
-    </div>
+    <CommonView
+      isItemLoaded={isItemLoaded}
+      totalCount={totalCount}
+      loadMoreItems={loadMoreItems}
+      NUM_COLUMNS={NUM_COLUMNS}
+      rowCount={rowCount}
+      data={huyaData}
+      title="虎牙"
+      urlName="huya"
+      ridName="profileRoom"
+      coverImgName="screenshot"
+      nickname="nick"
+      titleName="introduction"
+    ></CommonView>
   )
 }
 
